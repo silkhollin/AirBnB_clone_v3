f401c443225a7413f2e0386fb2ec1f4609c9b643
silkhollin <silcollinz@gmail.com>
2024-05-01 07:32:28 -0700

    fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -36,6 +36,6 @@ class TestConsoleDocs(unittest.TestCase):
     def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
         self.assertIsNot(HBNBCommand.__doc__, None,
-                         "HBNBCommand class needs the  docstring")
+                         "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
-                        "HBNBCommand class needs the docstring")
+                        "HBNBCommand class needs a docstring")
